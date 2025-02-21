2a50f8d4339c45662991def4b84fe80bf3bfccb3
이상민(SKCC) <sm.lee1@sk.com>
2025-02-21 10:20:48 +0900

    sync with server api by pagedList

--- src/pages/domains/admin/components/QuizSummary.vue
+++ src/pages/domains/admin/components/QuizSummary.vue
@@ -1,18 +1,22 @@
 <script setup lang="ts">
-import { reactive } from 'vue'
+import { reactive, ref, onMounted } from 'vue'
 import { ascendArray } from '@/scripts/utils'
 import apiCall from '@/scripts/api-call'
 import { SubjectSetting } from './_interfaces'
-import { getTimestamp } from '@/scripts/time-util'
+import { getDate, getTimestamp } from '@/scripts/time-util'
+import { notifyInfo } from '@/scripts/store-popups'
 
 const props = defineProps<{
     setting: SubjectSetting
 }>()
 
+const scoreDate = ref('')
+
 const table = reactive({
     headers: [
         { label: "질문ID", value: "quizId" },
         { label: "질문", value: "quizQuestion" },
+        { label: "선택항목", value: "quizOptions" },
         { label: "정답", value: "quizAnswer" },
         { label: "정답수", value: "correctCount" },
         { label: "오답수", value: "incorrectCount" },
@@ -22,19 +26,25 @@ const table = reactive({
 
 const showSummary = async () => {
     table.items.length = 0
-    const url = `/api/quiz/report/list?subjectId=${props.setting.id}`
-    const { body: list } = await apiCall.get(url, null, null)
-    if (list) {
-        table.items = list
+    const url = '/api/quiz/report/list'
+    const queryString = {
+        subjectId: props.setting.id,
+        date: scoreDate.value,
+    }
+
+    const { body: pagedList } = await apiCall.get(url, null, queryString)
+    if (pagedList.total > 0) {
+        table.items = pagedList.list
         ascendArray(table.items, ['correctCount', 'quizId'])
         console.log(table.items)
+    } else {
+        notifyInfo("Quiz 채점을 완료 하셨나요?")
     }
 }
 
 const hideSummary = () => {
     table.items.length = 0
 }
-
 
 const scoreQuiz = async () => {
     const url = '/api/applicant-quiz/score'
@@ -51,6 +61,9 @@ const downloadScore = async () => {
     await apiCall.download(url, null, null, `${props.setting.subjectName}-score-${getTimestamp()}`)
 }
 
+onMounted(() => {
+    scoreDate.value = getDate(null, '')
+})
 </script>
 
 <template>
@@ -60,6 +73,7 @@ const downloadScore = async () => {
                 <h4 class="fw-bold">Quiz 응답 현황</h4>
             </div>
             <div class="col d-flex justify-content-end">
+                <InlineInput class="me-1" v-model="scoreDate" type="text" placeholder="채점일(YYYYMMDD)" />
                 <button class="btn btn-sm btn-outline-danger me-1" @click="scoreQuiz">
                     Quiz 채점
                 </button>
